822fc1054a585865f1c1fd1ac17c4dce6ca39b76
andrejrohlik <andrej.rohlik@skole.hr>
2016-05-04 13:58:02 +0200

    login

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
-<?php session_start();
-if ($_SESSION['count']) {
-    $_SESSION['count'] = 0;
-} else {
-    $_SESSION['count']++;
-}
-?>
+<?php require('check.php'); ?>
 <?php require('db.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
